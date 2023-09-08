534d5eb806de7b2ccd0495b895557e404cf5f0a8
samara-kp <samaranoor0@gmail.com>
2023-09-08 13:23:37 +0530

    useMemo implemented

---
+++ Samara/useMemo.js
@@ -0,0 +1,41 @@
+const Child = (props) => {
+
+    console.log(`Child is rendering!!`);
+  
+    return (
+      <div className="ui very padded center aligned container segment">
+        <h4>Child :</h4>
+      </div>
+    );
+  }
+  
+  const App = () => {
+  
+    const [data, setData] = React.useState({ name: "Steve", age: 86 });
+
+    console.log('Parent is rendering');
+  
+    const memoizedfn = React.useCallback(() => dosomething(), []);
+    const memoizedData = React.useMemo(() => heavyComputation(), []);
+  
+    function dosomething() {
+      console.log("function called");
+    }
+
+    function heavyComputation() {
+        console.log('Do heavy computation')
+    }
+  
+    return (
+      <div className="ui very padded center aligned container segment">
+        <h3>From Parent:{data.name}</h3>
+        <button onClick={() => setData({ name: "Mike", age: 5 })}>
+          Change Parent Model
+        </button>
+        <Child method={memoizedData} />
+      </div>
+    );
+  };
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<App />);
